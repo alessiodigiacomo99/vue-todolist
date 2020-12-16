777be6f7dc30363824f622bdd37a8b1caac49b9e
alessio <alessiodg99.adg@gmail.com>
2020-12-16 12:33:59 +0100

    inserito task nella lista al click del bottone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
 /* Istruzioni:
-Create una todo list usando VueJS.
-Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS.
-Se non sapere che fare, di seguito trovate uno screenshot.
 Funzionalitá:
 La nostra todo list avrá alcune tasks di default predefinite
 L'utente puó inserire nuove tasks
@@ -9,3 +6,19 @@ Cliccando sulla "X" l'utente puó cancellare una task
 Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
 Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
 Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
+let root = new Vue ({
+    el: "#root",
+    data:{
+        list:[],
+        task: ""
+    },
+    methods:{
+        creaTask (){
+            if (this.task.length >= 3){
+                this.list.push(this.task);
+                this.task = "";
+
+            }
+        }
+    }
+})
